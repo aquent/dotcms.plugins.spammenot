2a01f06adf113bd965ebc6e5c0720e24904f699b
Christopher Falzone <cfalzone@aquent.com>
2017-09-21 15:11:53 -0400

    WEB-5220 - Checkstyle changes

--- src/main/java/com/aquent/viewtools/SpamMeNotActivator.java
+++ src/main/java/com/aquent/viewtools/SpamMeNotActivator.java
@@ -3,20 +3,24 @@ package com.aquent.viewtools;
 import org.osgi.framework.BundleContext;
 import com.dotmarketing.osgi.GenericBundleActivator;
 
+/**
+ * SpamMeNot Viewtool Activator.
+ * @author cfalzone
+ */
 public class SpamMeNotActivator extends GenericBundleActivator {
 
     @Override
-    public void start ( BundleContext bundleContext ) throws Exception {
+    public void start(BundleContext bundleContext) throws Exception {
 
         //Initializing services...
-        initializeServices( bundleContext );
+        initializeServices(bundleContext);
 
         //Registering the ViewTool service
-        registerViewToolService( bundleContext, new SpamMeNotInfo() );
+        registerViewToolService(bundleContext, new SpamMeNotInfo());
     }
 
     @Override
-    public void stop ( BundleContext bundleContext ) throws Exception {
+    public void stop(BundleContext bundleContext) throws Exception {
         unregisterViewToolServices();
     }
 
