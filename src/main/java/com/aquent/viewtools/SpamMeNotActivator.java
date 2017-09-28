29f5b795bf2ab85a593af7ea532916f1bae2fa5c
Christopher Falzone <cfalzone@aquent.com>
2017-09-28 09:56:33 -0400

    WEB-5220 - Setup Proper Logging

--- src/main/java/com/aquent/viewtools/SpamMeNotActivator.java
+++ src/main/java/com/aquent/viewtools/SpamMeNotActivator.java
@@ -1,6 +1,10 @@
 package com.aquent.viewtools;
 
 import org.osgi.framework.BundleContext;
+
+import com.dotcms.repackage.org.apache.logging.log4j.LogManager;
+import com.dotcms.repackage.org.apache.logging.log4j.core.LoggerContext;
+import com.dotmarketing.loggers.Log4jUtil;
 import com.dotmarketing.osgi.GenericBundleActivator;
 
 /**
@@ -8,9 +12,14 @@ import com.dotmarketing.osgi.GenericBundleActivator;
  * @author cfalzone
  */
 public class SpamMeNotActivator extends GenericBundleActivator {
+    private LoggerContext pluginLoggerContext;
 
     @Override
     public void start(BundleContext bundleContext) throws Exception {
+        // Setup Logger
+        LoggerContext dotcmsLoggerContext = Log4jUtil.getLoggerContext();
+        pluginLoggerContext = (LoggerContext) LogManager.getContext(this.getClass().getClassLoader(),
+            false, dotcmsLoggerContext, dotcmsLoggerContext.getConfigLocation());
 
         //Initializing services...
         initializeServices(bundleContext);
@@ -22,6 +31,9 @@ public class SpamMeNotActivator extends GenericBundleActivator {
     @Override
     public void stop(BundleContext bundleContext) throws Exception {
         unregisterViewToolServices();
+        unpublishBundleServices();
+        unregisterServices(bundleContext);
+        Log4jUtil.shutdown(pluginLoggerContext);
     }
 
 }
